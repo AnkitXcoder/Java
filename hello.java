f4ffeecf7017af24ec1fd412fb3dd92261a4f1ee
AnkitDecoder <ankitone02@gmail.com>
2024-10-12 11:10:48 +0530

    First commit

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ public class hello
 {
     public static void main(String []args)
     {
-      System.out.println("hello");
-      System.out.print("Ankitbhai");
+      System.out.print("hello");         //print hello
+      System.out.println("Ankit");       //print Ankit on same line of hello and[ move cursor to next line]
+      System.out.print("Ankitbhai");     //print Ankitbhai on the line of Ankit
     }
 }
