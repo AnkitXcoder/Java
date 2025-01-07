91b8da37fb9ba41f73f4f845fa69a681c7748ff6
AnkitX <ankitone02@gmail.com>
2025-01-07 22:53:24 +0530

    adding array

---
+++ DSA/array.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public  class array{
+         
+    public static void main(String[] args) {
+          
+        int nums []={ 1,0,3,4};
+
+        for ( int i=0; i<=3;i++)
+        {
+            System.out.println(nums[i]);
+        }
+// creating array for Strings
+        String name[]={"Ankit","Ankita","Anibha","Ancha"};
+        
+        for ( int i=0; i<=3;i++)
+        {
+            System.out.println(name[i]);
+        }
+
+    }
+
+}
